396f1eba0b2e301a1f3d777b739741c0d42e55bd
Ajrdn <ajrdn1276@gmail.com>
2022-03-30 19:22:08 +0900

    VariableTest

---
+++ Chapter 01/ch02/Java/VariableTest.java
@@ -0,0 +1,15 @@
+package ch02;
+
+public class VariableTest {
+
+	public static void main(String[] args) {
+		int age, count;
+		age = 17;
+		
+		int level = 245;
+		System.out.println(age);
+		System.out.println(level);
+		
+	}
+
+}
