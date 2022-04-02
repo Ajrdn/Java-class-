017ee99f51301eeb6a61c26ba474300874c8a0e4
Ajrdn <ajrdn1276@gmail.com>
2022-04-02 19:57:59 +0900

    CharacterTest

---
+++ Chapter 01/ch05/Java/CharacterTest.java
@@ -0,0 +1,22 @@
+package ch05;
+
+public class CharacterTest {
+
+	public static void main(String[] args) {
+		char ch1 = 'A';
+		System.out.println(ch1);
+		System.out.println((int)ch1);
+		
+		char ch2 = 66;
+		System.out.println(ch2);
+		
+		int ch3 = 67;
+		System.out.println(ch3);
+		System.out.println((char)ch3);
+		
+		char ch5 = 'วั';
+		char ch6 = '\uD55C';
+
+	}
+
+}
