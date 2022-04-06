c8fdb9b0aae5680e3ba65920e4a13c691e6f1002
Ajrdn <ajrdn1276@gmail.com>
2022-04-06 17:45:01 +0900

    OperatorTest

---
+++ Chapter 01/ch09/Java/OperatorTest.java
@@ -0,0 +1,17 @@
+package ch09;
+
+public class OperatorTest {
+
+	public static void main(String[] args) {
+		int num1 = 10;
+		int i = 2;
+		
+		boolean value = ((num1 += 10) < 10) && ((i += 2) < 10);
+		
+		System.out.println(value);
+		System.out.println(num1);
+		System.out.println(i);
+
+	}
+
+}
