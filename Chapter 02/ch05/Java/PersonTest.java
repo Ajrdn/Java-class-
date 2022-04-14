9ea88c498500931e9b2a3549d98bc769001ca33d
Ajrdn <ajrdn1276@gmail.com>
2022-04-14 12:13:50 +0900

    PersonTest

---
+++ Chapter 02/ch05/Java/PersonTest.java
@@ -0,0 +1,11 @@
+package ch05;
+
+public class PersonTest {
+
+	public static void main(String[] args) {
+		Person mrchoi = new Person (180, "mrchoi", 83, "³²¼º", 26);
+		
+		mrchoi.showPersonInfo();
+	}
+
+}
