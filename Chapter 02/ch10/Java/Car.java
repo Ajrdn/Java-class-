7b6055a4ca77a248ec5b7f343510dcf4021ba95c
Ajrdn <ajrdn1276@gmail.com>
2022-04-19 20:34:40 +0900

    Car

---
+++ Chapter 02/ch10/Java/Car.java
@@ -0,0 +1,14 @@
+package ch10;
+
+public class Car {
+	
+	String name;
+	
+	public Car(String name) {
+		this.name = name;
+	}
+	
+	public int take(GasStation gasStation, int liter) {
+		return gasStation.oiling(liter);
+	}
+}
