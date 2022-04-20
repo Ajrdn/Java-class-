06821c46c252f07f25c4dedb0a2272424d575412
Ajrdn <ajrdn1276@gmail.com>
2022-04-20 17:16:10 +0900

    EmployeeTest

---
+++ Chapter 02/ch11/Java/EmployeeTest.java
@@ -0,0 +1,15 @@
+package ch11;
+
+public class EmployeeTest {
+
+	public static void main(String[] args) {
+		
+		Employee e1 = new Employee("choi");
+		Employee e2 = new Employee("kim");
+		
+		System.out.println(e1.getEmployeeNumber());
+		System.out.println(e2.getEmployeeNumber());
+		
+	}
+
+}
