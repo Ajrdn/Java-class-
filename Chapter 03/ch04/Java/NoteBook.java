cbf10a8e4bc0f32a2cdda2a1960a5a2baee0cf17
Ajrdn <ajrdn1276@gmail.com>
2022-04-27 15:38:09 +0900

    NoteBook

---
+++ Chapter 03/ch04/Java/NoteBook.java
@@ -0,0 +1,12 @@
+package ch04;
+
+public abstract class NoteBook extends Computer{
+
+   @Override
+   public void typing() {
+      
+      System.out.println("NoteBook typing");
+      
+   }
+
+}
