b2de02207f80fd2959d10cb6f636b00d1b8928f0
Ajrdn <ajrdn1276@gmail.com>
2022-04-28 13:39:14 +0900

    ManualCar

---
+++ Chapter 03/ch05/Java/ManualCar.java
@@ -0,0 +1,19 @@
+package ch05;
+
+public class ManualCar extends Car{
+
+	@Override
+	public void drive() {
+		System.out.println("기어봉을 P->D");
+		System.out.println("액셀을 밟는다.");
+		System.out.println("핸들을 조향한다.");
+	}
+
+	@Override
+	public void stop() {
+		System.out.println("브레이크를 밟는다.");
+		System.out.println("기어봉을 D->P");
+	}
+	
+	
+}
