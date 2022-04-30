df0ceca5386db7b0e27111a42efea4d996e0f79e
Ajrdn <ajrdn1276@gmail.com>
2022-04-30 18:38:49 +0900

    PriorityAll

---
+++ Chapter 03/ch07/Java/PriorityAll.java
@@ -0,0 +1,14 @@
+package ch07;
+
+public class PriorityAll implements Scheduler{
+
+	@Override
+	public void getNextCall() {
+		System.out.println("우선순위가 높은 고객 순으로 대기열에서 가져옵니다.");
+	}
+
+	@Override
+	public void sendCallToAgent() {
+		System.out.println("상담 업무 스킬이 높은 상담원에게 배정합니다.");
+	}
+}
