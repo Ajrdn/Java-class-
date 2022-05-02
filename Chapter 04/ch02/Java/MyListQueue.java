70df438ff2fe30b02aa346ee8f6be7167ca08791
Ajrdn <ajrdn1276@gmail.com>
2022-05-02 17:48:44 +0900

    MyListQueue

---
+++ Chapter 04/ch02/Java/MyListQueue.java
@@ -0,0 +1,58 @@
+package ch02;
+
+import ch01.MyLinkedList;
+import ch01.MyListNode;
+
+public class MyListQueue extends MyLinkedList implements IQueue{
+	
+	private MyListNode front;
+	private MyListNode rear;
+	
+	public MyListQueue() {
+		front = null;
+		rear = null;
+	}
+
+	public void enQueue(String data) {
+		MyListNode newNode;
+		if(isEmpty()) {
+			newNode = addElement(data);
+			front = newNode;
+			rear = newNode;
+		}
+		else {
+			newNode = addElement(data);
+			rear.next = newNode;
+			rear = newNode;
+		}
+		
+	}
+
+	public String deQueue() {
+		if(isEmpty()) {
+			System.out.println("현재 큐가 비어있습니다.");
+			return null;
+		}
+		
+		String data = front.getData();
+		
+		front = front.next;
+		if(front == null) {
+			rear = null;
+		}
+		
+		return data;
+	}
+
+	public void printAll() {
+		MyListNode temp = front;
+		while(temp != null) {
+			System.out.print(temp.getData());
+			if(temp.next != null)
+				System.out.print(" -> ");
+			temp = temp.next;
+		}
+		System.out.println("");
+	}
+	
+}
